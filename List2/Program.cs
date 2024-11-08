3900bbbc51c1cdf2c865503897f86ca995f5fb56
gunessumre <73123396+gunessumre@users.noreply.github.com>
2024-11-08 14:03:07 +0300

    jkhj

---
+++ List2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> kahveIsimleri = new List<string>();
+
+        Console.WriteLine("Lütfen 5 kahve ismi giriniz:");
+        
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Kahve {i}: ");
+            string kahveAdi = Console.ReadLine();
+            kahveIsimleri.Add(kahveAdi);
+        }
+        Console.WriteLine("\nGirdiğiniz kahve isimleri:");
+        foreach (string kahve in kahveIsimleri)
+        {
+            Console.WriteLine(kahve);
+        }
+    }
+}
